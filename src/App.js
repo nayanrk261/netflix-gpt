88a2f7d5b9cab62159fed6c31dcb239183187a43
NYN KHANDELWAL <your_email@example.com>
2026-03-20 21:43:20 +0530

    day1,52M..routing,header,body,browse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import Body from "./components/Body"
 function App() {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-green-500 border-4 border-red-500 p-6">
-        Tailwind is Working 🚀
-      </h1>
-    </div>
+    <Body/>
   );
 }
 
